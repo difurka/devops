88974f54e9120b1ea044ab8fa15995bb2f0c5558
Katya <e.balakina@g.nsu.ru>
2023-01-18 12:34:44 +0700

    end of part2

--- MON1/02/main.sh
+++ MON1/02/main.sh
@@ -8,7 +8,6 @@ function round_2 {
 echo $(python3 -c "print(round(($1)/1024.0/1024.0, 2))")
 }
 
-
 ip=`hostname -I`
 ram_total_in_mb=`free -t | grep Total | awk -F '[[:space:]][[:space:]]+' '{print$2}'`
 ram_total=$(round_3 $ram_total_in_mb)
@@ -30,44 +29,23 @@ ram_free=$(round_2 $root_free_in_mb)
 
 text="
 HOSTNAME: $(hostname)
-TIMEZONE: $(cat /etc/timezone) $(date +%Z)
-USER: $(whoami)
-OS: $(cat /etc/issue | cut -d'\' -f1)
-DATE: $(date +%d) $(date +%b) $(date +%Y) $(date +%T)
-UPTIME: $(uptime -p)
-UPTIME_SEC: $(cat /proc/uptime | cut -d' ' -f1)
-IP: $ip
-MASK: $(ifconfig | grep $ip | awk -F 'netmask'  '{print $2}' | cut -d ' ' -f2)
-GATEWAY: $(ip route | grep default | cut -d ' ' -f3)
-RAM_TOTAL: $ram_total GB
-RAM_USED: $ram_used GB
-RAM_FREE: $ram_free GB
-PACE_ROOT: $ram_total MB
-SPACE_ROOT_USED: $ram_used MB
-SPACE_ROOT_FREE: $ram_free MB
+\nTIMEZONE: $(cat /etc/timezone) $(date +%Z)
+\nUSER: $(whoami)
+\nOS: $(cat /etc/issue | cut -d'\' -f1)
+\nDATE: $(date +%d) $(date +%b) $(date +%Y) $(date +%T)
+\nUPTIME: $(uptime -p)
+\nUPTIME_SEC: $(cat /proc/uptime | cut -d' ' -f1)
+\nIP: $ip
+\nMASK: $(ifconfig | grep $ip | awk -F 'netmask'  '{print $2}' | cut -d ' ' -f2)
+\nGATEWAY: $(ip route | grep default | cut -d ' ' -f3)
+\nRAM_TOTAL: $ram_total GB
+\nRAM_USED: $ram_used GB
+\nRAM_FREE: $ram_free GB
+\nPACE_ROOT: $ram_total MB
+\nSPACE_ROOT_USED: $ram_used MB
+\nSPACE_ROOT_FREE: $ram_free MB
 "
-
-
-# echo "HOSTNAME: $HOSTNAME"
-# echo "TIMEZONE: $(cat /etc/timezone) $(date +%Z)"
-# echo "USER: $(whoami)"
-# echo "OS: $(cat /etc/issue | cut -d'\' -f1)"
-# echo "DATE: $(date +%d) $(date +%b) $(date +%Y) $(date +%T)"
-# echo "UPTIME: $(uptime -p)"
-# echo "UPTIME_SEC: $(cat /proc/uptime | cut -d' ' -f1)"
-# echo "IP: $ip"
-# echo "MASK: $(ifconfig | grep $ip | awk -F 'netmask'  '{print $2}' | cut -d ' ' -f2)"
-# echo "GATEWAY: $(ip route | grep default | cut -d ' ' -f3)"
-# echo "RAM_TOTAL: $ram_total GB"
-# echo "RAM_USED: $ram_used GB"
-# echo "RAM_FREE: $ram_free GB"
-# echo "PACE_ROOT: $ram_total MB"
-# echo "SPACE_ROOT_USED: $ram_used MB"
-# echo "SPACE_ROOT_FREE: $ram_free MB"
-
-echo $text
-
-temp="ttt"
+echo -e $text
 
 read -p "
 ---
@@ -76,9 +54,7 @@ writing the data to a file?  " answer
 if [[ $answer = "y" ]] || [[ $answer = "Y" ]]; 
 then
 file_name="$(date +%d)_$(date +%m)_$(date +%Y)_$(date +%H)_$(date +%M)_$(date +%S).status"
-
-temp="ttt"
-$(temp) >> $file_name
+echo -e $text >> $file_name
 fi
 
 
