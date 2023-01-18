068db9848ebfc29cd295cd1438e407f271b5a35e
Katya <e.balakina@g.nsu.ru>
2023-01-18 11:54:27 +0700

    added part2

--- MON1/02/main.sh
+++ MON1/02/main.sh
@@ -2,47 +2,59 @@
 
 echo "HOSTNAME: $HOSTNAME"
 
-tz=`cat /etc/timezone`
-hours=`date +%Z`
-echo "TIMEZONE: $tz $hours"
+echo "TIMEZONE: $(cat /etc/timezone) $(date +%Z)"
 
 echo "USER: $(whoami)"
 
-os_name=`cat /etc/issue | cut -d'\' -f1`   #`grep PRETTY_NAME /etc/os-release`
-echo "OS: $os_name"
+echo "OS: $(cat /etc/issue | cut -d'\' -f1)"
 
-day=`date +%d`
-month=`date +%b`
-year=`date +%Y`
-time=`date +%T`
-echo "DATE: $day $month $year $time"
+echo "DATE: $(date +%d) $(date +%b) $(date +%Y) $(date +%T)"
 
 echo "UPTIME: $(uptime -p)"
 
-time_in_sec=`cat /proc/uptime | cut -d' ' -f1`
-echo "UPTIME_SEC: $time_in_sec"
+echo "UPTIME_SEC: $(cat /proc/uptime | cut -d' ' -f1)"
 
-# ip=`ifconfig eth0 | grep "inet addr" | head -n 1 | cut -d : -f 2 | cut -d " " -f 1`
 ip=`hostname -I`
 echo "IP: $ip"
 
-mask=`ifconfig enp0s3 | grep $ip | head -n 1 | cut -d ' ' -f 13`
-echo "MASK: $mask"
+echo "MASK: $(ifconfig | grep $ip | awk -F 'netmask'  '{print $2}' | cut -d ' ' -f2)"
 
-gateway=`ip route | grep default | cut -d ' ' -f3`
-echo "GATEWAY: $gateway"
+echo "GATEWAY: $(ip route | grep default | cut -d ' ' -f3)"
 
-echo "RAM_TOTAL: $(free -g -h -t | grep Total | cut -d ' ' -f 9)"
+function round_3 {
+echo $(python3 -c "print(round($1/1024.0/1024, 3))")
+}
 
-echo "RAM_USED: $(free -g -h -t | grep Total | cut -d ' ' -f 16)"
+ram_total_in_mb=`free -t | grep Total | awk -F '[[:space:]][[:space:]]+' '{print$2}'`
+ram_total=$(round_3 $ram_total_in_mb)
+echo "RAM_TOTAL: $ram_total GB"
 
-echo "RAM_FREE: $(free -g -h -t | grep Total | cut -d ' ' -f 9)"
+ram_used_in_mb=`free -t | grep Total | awk -F '[[:space:]][[:space:]]+' '{print$3}'`
+ram_used=$(round_3 $ram_used_in_mb)
+echo "RAM_USED: $ram_used GB"
 
-# echo "SPACE_ROOT: $user"
+ram_free_in_mb=`free -t | grep Total | awk -F '[[:space:]][[:space:]]+' '{print$4}'`
+ram_free=$(round_3 $ram_free_in_mb)
+echo "RAM_FREE: $ram_free GB"
 
-# echo "SPACE_ROOT_USED: $user"
 
-# echo "SPACE_ROOT_FREE: $user"
+function round_2 {
+echo $(python3 -c "print(round(($1)/1024.0/1024.0, 2))")
+}
+root=`df /boot/ -B1`
+
+root_total_in_mb=`df /boot/ -B1 | grep / | awk -F '[[:space:]][[:space:]]+' '{print$2}' | cut -d ' ' -f1`
+ram_total=$(round_2 $root_total_in_mb)
+echo "PACE_ROOT: $ram_total MB"
+
+root_used_in_mb=`df /boot/ -B1 | grep / | awk -F '[[:space:]][[:space:]]+' '{print$2}' | cut -d ' ' -f2`
+ram_used=$(round_2 $root_used_in_mb)
+echo "SPACE_ROOT_USED: $ram_used MB"
+
+root_free_in_mb=`df /boot/ -B1 | grep / | awk -F '[[:space:]][[:space:]]+' '{print$2}' | cut -d ' ' -f3`
+ram_free=$(round_2 $root_free_in_mb)
+echo "SPACE_ROOT_FREE: $ram_free MB"
+
 
 
 
